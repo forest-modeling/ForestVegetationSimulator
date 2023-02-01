0cf74b1a420f65deccb8d8de24d34324ce2751f0
tharen-odf <tod.h.haren@odf.oregon.gov>
2023-02-01 10:41:34 -0800

    Fixed format comment character

--- volume/mrules_mod.f
+++ volume/mrules_mod.f
@@ -1,33 +1,33 @@
       MODULE MRULES_MOD
 
-!     Module to store merch rules
+C     Module to store merch rules
 
-!     Created TDH 06/17/09
-!     Revised ... DD/MM/YY 
+C     Created TDH 06/17/09
+C     Revised ... DD/MM/YY
 
-!     Class for passing/receiving merch rules in calls from C++
+C     Class for passing/receiving merch rules in calls from C++
       TYPE MERCHRULES
       SEQUENCE
         INTEGER        EVOD
-        INTEGER        OPT     
-        
-        
+        INTEGER        OPT
+
+
         REAL           MAXLEN
         REAL           MINLEN
         REAL           MINLENT
         REAL           MERCHL
         REAL           MTOPP
         REAL           MTOPS
-        
+
         REAL           STUMP
         REAL           TRIM
         REAL           BTR
         REAL           DBTBH
         REAL           MINBFD
-        
-        CHARACTER*1    COR 
-        
+
+        CHARACTER*1    COR
+
       END TYPE MERCHRULES
-   
+
 
       END MODULE MRULES_MOD
